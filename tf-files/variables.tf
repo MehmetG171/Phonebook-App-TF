b0c9c673a8ad2880852dc9592e62135b1d61f261
MehmetG171 <68816334+MehmetG171@users.noreply.github.com>
2024-03-30 21:54:12 +0300

    Add files via upload

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "MehmetG171"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "New"
+}
+
+variable "hosted-zone" {
+  default = "mngcloud.click"
+}
